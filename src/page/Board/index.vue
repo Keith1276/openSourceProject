6dd171adc34a01ead471083bd07d37098e6c32b7
berry0303 <142763755+berry0303@users.noreply.github.com>
2024-12-05 19:09:37 +0800

    让Board显示出来

--- src/page/Board/index.vue
+++ src/page/Board/index.vue
@@ -1,37 +1,28 @@
 <template>
   <el-container>
-
-    <el-header>
-      <search></search>
-    </el-header>
-
-      <el-main>
-        <div class="left-container">
-          <p>这里左端放热点开源项目</p>
-        </div>
-
-        <div class="right-container">
-          <p>这里右端放热点开源开发者</p>
-        </div>>
-      </el-main>
+    <search></search>
+    <el-header> </el-header>
+    <el-main>
+      <div class="left-container">
+        <p>这里左端放热点开源项目</p>
+      </div>
+      <div class="right-container">
+        <p>这里右端放热点开源开发者</p>
+      </div>
+      >
+    </el-main>
   </el-container>
-
 </template>
 
 <script>
+import search from "./search.vue";
 import router from "@/router/index.js";
-export default
-{
-  name:"Board",
-  data()
-  {
-    return{
-
-    };
+export default {
+  name: "Board",
+  components: { search },
+  data() {
+    return {};
   },
 };
-
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
