927774dccf88a1d2395e1987ad867ffeb0cb096f
Keith1276 <141840126+Keith1276@users.noreply.github.com>
2024-12-13 16:40:45 +0800

    调axios头

--- src/api/searchRepos.ts
+++ src/api/searchRepos.ts
@@ -28,7 +28,7 @@ export async function searchRepos(data : {
 } ) : Promise<void> {
     try {
 
-        const response = await axios.post('https://api.github.com/search/repositories',data);
+        const response = await axios.post('http://localhost:8085/search/repositories',data);
         //const response = await axios.get('https://api.github.com/search/users', { params });
         if (response.status === 200) {
 
