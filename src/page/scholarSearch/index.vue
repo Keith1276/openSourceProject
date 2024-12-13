5c15936d55e1f398bd42a322b67ea28292654a04
berry0303 <142763755+berry0303@users.noreply.github.com>
2024-12-13 16:41:27 +0800

    学者搜索接口

--- src/page/scholarSearch/index.vue
+++ src/page/scholarSearch/index.vue
@@ -118,14 +118,14 @@
                           color: #4994c4;
                           font-size: small;
                         "
-                        >{{ region.public_repos }}</b
+                        >{{ region.publicRepos }}</b
                       >
                     </p>
                   </div>
                 </div>
                 <div class="region-top-end">
                   <a
-                    :href="region.html_url"
+                    :href="region.htmlUrl"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
@@ -158,9 +158,10 @@
 </template>
 <script lang="ts">
 import search from "./search.vue";
-import { ref, computed, onMounted, watch } from "vue";
+import { ref, computed, onMounted, watch, watchEffect, reactive } from "vue";
 import router from "@/router/index.js";
 import { useRoute } from "vue-router";
+import { getScholarData } from "@/api/scholarSearch";
 export default {
   components: { search },
   setup() {
@@ -169,26 +170,40 @@ export default {
     const input3 = ref("");
     const select = ref("");
     const route = useRoute();
+    const Keywords = ref<string[]>([]);
+
+    watchEffect(() => {
+      const value = input3.value;
+      const tempArray = reactive(value.split(" ").filter(Boolean));
+      Keywords.value = tempArray;
+    });
     const regions = ref([
       {
         id: 1,
+        userId: 1,
         login: "Coke_And_1ce",
+        avatarUrl: "",
+        htmlUrl: "https://github.com/",
         email: "skyshipwc@163.com",
         followers: 1,
-        public_repos: 1,
-        html_url: "https://github.com/",
-      },
-      {
-        id: 2,
-        login: "Berry",
-        email: "skyshipwc@163.com",
-        followers: 1,
-        public_repos: 1,
-        html_url: "https://github.com/",
+        publicRepos: 1,
+        score: 1,
+        matchScore: 1,
       },
     ]);
-    const handleSearch = () => {
-      console.log("click success");
+    const handleSearch = async () => {
+      const condition = {
+        keywords: Keywords.value,
+        pageNum: pagination.value.currentPage,
+        pageSize: pagination.value.pageSize,
+      };
+      try {
+        const data = await getScholarData(condition);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching scholar data:", error);
+        regions.value = [];
+      }
     };
     const jumpPersonal = () => {
       console.log("jumpPersonal click success");
